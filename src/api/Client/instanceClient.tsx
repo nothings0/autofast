d526071f7d11d9d491afda39e1a7878be11b4f7b
Nothings0 <nhanphamx3@gmail.com>
2023-12-16 20:17:28 +0700

    update db

--- src/api/Client/instanceClient.tsx
+++ src/api/Client/instanceClient.tsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instanceClient = axios.create({
-    baseURL: 'http://localhost:3000'
-})
+  baseURL: "https://autofast.vercel.app",
+});
 
 export default instanceClient;
